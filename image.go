5b7a91001a07542d1c09d6acc73d5dea8e82d40b
secondarykey <secondarykey@gmail.com>
2016-02-03 00:17:26 +0900

    Zanen

--- image.go
+++ image.go
@@ -13,21 +13,18 @@ import (
 	"github.com/google/gxui"
 	"github.com/google/gxui/drivers/gl"
 	"github.com/google/gxui/themes/dark"
-	"github.com/secondarykey/go-opencv/opencv"
+	"github.com/lazywei/go-opencv/opencv"
 )
 
 func appMain(driver gxui.Driver) {
 
-	filename := "/home/secondarykey/Videos/matrix2.mp4"
+	filename := "matrix2.mp4"
 	capt := opencv.NewFileCapture(filename)
 	if capt == nil {
 		panic("can not open video")
 	}
 	defer capt.Release()
 
-	//
-	//fps := int(capt.GetProperty(opencv.CV_CAP_PROP_FPS))
-	//frames := int(capt.GetProperty(opencv.CV_CAP_PROP_FRAME_COUNT))
 	w := int(capt.GetProperty(opencv.CV_CAP_PROP_FRAME_WIDTH))
 	h := int(capt.GetProperty(opencv.CV_CAP_PROP_FRAME_HEIGHT))
 
@@ -41,33 +38,13 @@ func appMain(driver gxui.Driver) {
 	rect := image.Rect(0, 0, w, h)
 	rgba := image.NewRGBA(rect)
 
-	window.OnClose(driver.Terminate)
-
-	// loop  ReDraw
-
 	cvImage := capt.QueryFrame()
-	frame_pos := int(capt.GetProperty(opencv.CV_CAP_PROP_POS_FRAMES))
-	if frame_pos >= frames {
-		break
-	}
-
-	/*
-		type Image interface {
-		    image.Image
-		    Set(x, y int, c color.Color)
-		}
-	*/
-
-	/*
-	   func Draw(dst Image, r image.Rectangle, src image.Image, sp image.Point, op Op)
-	   -> draw.Draw(rgba, source.Bounds(), source, image.ZP, draw.Src)
-	*/
-
 	draw.Draw(rgba, rect, cvImage.ToImage(), image.ZP, draw.Src)
 
 	texture := driver.CreateTexture(rgba, 1)
 	imgWd.SetTexture(texture)
 
+	window.OnClose(driver.Terminate)
 }
 
 func main() {
